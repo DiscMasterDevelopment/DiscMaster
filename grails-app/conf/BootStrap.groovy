bf86f77f259b55be184205c7a61c37312953cb1c
helq <linuxero789@gmail.com>
2016-04-06 18:29:58 -0500

    Adding Bootstrap: Adding an User and some Products to the db

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,91 @@
+import discmaster.User
+import discmaster.Product
+import discmaster.Description
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if(Environment.current == Environment.DEVELOPMENT) {
+            // Adding Products
+            def products = [
+                [
+                    description: "The best description",
+                    product: [
+                        name: "Disc 1",
+                        price: 400,
+                        discount: 0,
+                        totalInStorage: 21,
+                        limitPerUser: 20,
+                        added: new Date()
+                    ]
+                ],
+                [
+                    description: "The 2nd best description",
+                    product: [
+                        name: "Disc 42",
+                        price: 350,
+                        discount: 0.3,
+                        totalInStorage: 21,
+                        limitPerUser: 20,
+                        added: new Date()
+                    ]
+                ],
+                [
+                    description: "A good description",
+                    product: [
+                        name: "Disc 13",
+                        price: 200,
+                        discount: 0,
+                        totalInStorage: 0,
+                        limitPerUser: 20,
+                        added: new Date()
+                    ]
+                ],
+                [
+                    description: "Description of a product",
+                    product: [
+                        name: "XXI Disc",
+                        price: 10,
+                        discount: 0.3,
+                        totalInStorage: 20,
+                        limitPerUser: 20,
+                        added: new Date()
+                    ]
+                ],
+                [
+                    description: "Üsing this things üäë",
+                    product: [
+                        name: "Disc pi^2",
+                        price: 27.2,
+                        discount: 0.2,
+                        totalInStorage: 20,
+                        limitPerUser: 20,
+                        added: new Date()
+                    ]
+                ],
+            ]
+
+            products.each { pnd ->
+                def d = new Description(description: pnd["description"])
+                d.save()
+                def p = new Product(pnd["product"] + [description: d])
+                p.save()
+            }
+            
+            // Adding users
+
+            def user = new User(
+                name: "john",
+                phone: "123456789",
+                realName: "John (Kyon) Smith",
+                email: "anemail@candykingdom.ck",
+                password: "123456789",
+                age: 18
+            )
+
+            user.save(flush: true)
+        }
     }
     def destroy = {
     }
