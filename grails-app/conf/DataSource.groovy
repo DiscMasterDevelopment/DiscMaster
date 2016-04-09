a793ed00f00cdf4085d3010de1da22f7ba6349a9
seacabezasma <seacabezasma@unal.edu.co>
2016-04-08 20:06:50 -0500

    init

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -31,7 +31,17 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+
+            //Configuracion para Heroku
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+
+            //url por defecto, descomentar para conf por defecto
+            //url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
