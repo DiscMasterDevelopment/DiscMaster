efe20d949f52cf6b42641b2921dfe24d40ef6185
helq <linuxero789@gmail.com>
2016-04-10 01:53:08 -0500

    Adding simple Address Domain Class test

--- test/unit/discmaster/AddressSpec.groovy
+++ test/unit/discmaster/AddressSpec.groovy
@@ -9,12 +9,34 @@ import spock.lang.Specification
 @TestFor(Address)
 class AddressSpec extends Specification {
 
+    def fields = [
+        city:    "Bogotá",
+        address: "Fake Street 123",
+        lastTimeUsed: new Date(),
+        contactPhone: "34212345678",
+        contactName: "John Smithiests",
+        postalCode: 43523
+    ]
+
     def setup() {
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "Creating a simple address"() {
+        given: "valid parameters"
+        def address = new Address(fields)
+
+        expect: "creating class without any problem"
+        address.validate()
+    }
+
+    void "Creating an invalid address"() {
+        given: "an invalid address"
+        def address = new Address()
+
+        expect: "a failure while saving"
+        !address.validate()
     }
 }
