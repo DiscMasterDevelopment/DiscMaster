972e5548d33d62d000d89bd44107fb5b73e8206d
Daniel <dauchamochaq@unal.edu.co>
2016-04-09 21:37:58 -0500

    Adding password file to Administrator domain class

--- grails-app/domain/discmaster/Administrator.groovy
+++ grails-app/domain/discmaster/Administrator.groovy
@@ -4,9 +4,15 @@ class Administrator {
 
     String name
     String phone
+    String password
 
     static constraints = {
         name nullable: false, matches: /DiscMaster_.+/, unique: true
         phone nullable: true
+        password blank: false, nullable: false, size: 8..15, validator: {val, obj ->
+            if (val?.equalsIgnoreCase(obj.name)){
+                return false
+            }
+        }
     }
 }
