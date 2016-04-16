b0d0311e1dbdf4ff37c437aad0e249731e1175be
helq <linuxero789@gmail.com>
2016-04-16 12:55:45 -0500

    Adding test for Administrator and updating test for Address

--- test/unit/discmaster/AddressSpec.groovy
+++ test/unit/discmaster/AddressSpec.groovy
@@ -2,6 +2,7 @@ package discmaster
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,7 +10,7 @@ import spock.lang.Specification
 @TestFor(Address)
 class AddressSpec extends Specification {
 
-    def fields = [
+    static def fields = [
         city:    "Bogotá",
         address: "Fake Street 123",
         lastTimeUsed: new Date(),
@@ -24,19 +25,29 @@ class AddressSpec extends Specification {
     def cleanup() {
     }
 
-    void "Creating a simple address"() {
-        given: "valid parameters"
+    void "Creating a simple Address"() {
+        given: "creating Address from valid parameters"
         def address = new Address(fields)
 
-        expect: "creating class without any problem"
+        expect: "validating object without a problem without any problem"
         address.validate()
     }
 
-    void "Creating an invalid address"() {
-        given: "an invalid address"
+    void "Creating an (invalid) empty Address"() {
+        given: "creating Address with no parameters"
         def address = new Address()
 
         expect: "a failure while saving"
         !address.validate()
+    }
+
+    @Unroll
+    void "Creating (invalid) addresses without a field"() {
+        expect: "failure on validation"
+        def address = new Address( TestHelpers.removeAField(fields, fieldToRemove) )
+        !address.validate()
+
+        where: "each field to be removed is passed one at the time"
+        fieldToRemove << ["city", "address", "lastTimeUsed", "contactPhone", "contactName", "postalCode"]
     }
 }
